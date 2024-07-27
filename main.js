e1d1350db824ddd01d13743e8145b142683afd93
Georgi Zarkya <zmeyfon@list.ru>
2024-07-27 21:09:49 +0300

    простые условия. 1 строка

--- main.js
+++ main.js
@@ -1,40 +1,18 @@
-// Даны два числа. Выведите:
-// - Равны ли они
-// - Одинаковое ли в них количество цифр
-// - Большее из чисел
-// - Меньшее из чисел
-// - Делится ли одно на другое без остатка
-let first_number = 10;
-let second_number = 15;
+` Простые условия. Одна строка.
 
-if (first_number == second_number) {
-    console.log(`${first_number} равке ${second_number}`);
-} else{
-    console.log(`${first_number} не равен ${second_number}`);
-}
+    Дана строка. Выведите:
 
-if (first_number.toString().length == second_number.toString().length) {
-    console.log("Содержится одинаковое количество цифр");
-} else{
-    console.log("Количество цифр не одинаково");
-}
-
-if (first_number > second_number) {
-    console.log(`Большее из чисел:${first_number}`);
-} else if (first_number < second_number)
+- Первый символ, если длина строки четная
+- Последний символ, если длина строки нечетная
+- Предпоследний символ, если длина строки нечетная и строка оканчивается на букву “a”
+`
+const name = 'Ruslan';
+if (name.length % 2 == 0) {
+    console.log(name.charAt(0));
+} else if (name.length % 2 == 1)
 {
-    console.log(`Большее из чисел:${second_number}`);
-}
-
-if (first_number > second_number) {
-    console.log(`Меньшее из чисел:${second_number}`);
-} else if (first_number < second_number)
+    console.log(name.slice(-1));
+} else if (name.length % 2 == 1 && name.slice(-1) == 'a')
 {
-    console.log(`Меньшее из чисел:${first_number}`);
-}
-
-if (first_number % second_number == 0) {
-    console.log("Числа делятся без остатка");
-} else {
-    console.log("Делятся с остатком");
+    console.log(name.slice(-2, -1));
 }
