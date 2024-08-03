2b3767ddac563a7c318fd2e6df6d26f7efbd7e4a
Georgi Zarkya <zmeyfon@list.ru>
2024-08-03 20:48:38 +0300

    Кости

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-// Запишите в переменную случайное число используя Math.random(), в цикле while обновляйте его на новое случайно число,
-// пока не выпадет число более 0.95, посчитайте общее количество попыток и выведите их
-let rnumber = Math.random();
+// Запишите в две переменные два случайных целых числа от 1 до 6, в цикле while обновляйте
+// оба числа, пока не выпадет дубль (два одинаковых числа), посчитайте количество попыток и выведите их
+const min = 1;
+const max = 6;
 let count = 0;
-while (rnumber < 0.95) {
-  rnumber = Math.random();
+let randomnum1 = Math.floor(Math.random() * (max - min + 1)) + min;
+let randomnum2 = Math.floor(Math.random() * (max - min + 1)) + min;
+while (randomnum1 !== randomnum2) {
+  randomnum1 = Math.floor(Math.random() * (max - min + 1)) + min;
+  randomnum2 = Math.floor(Math.random() * (max - min + 1)) + min;
   count++;
 }
 console.log(`Общее количество попыток: ${count}`);
