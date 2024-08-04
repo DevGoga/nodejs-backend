0199f14e5dabcd909cacad41520da553d423c831
Georgi Zarkya <zmeyfon@list.ru>
2024-08-04 21:48:09 +0300

    Вывести вторую половину

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-// Дан массив строк. Вывести только первую половину массива (если длина не делится нацело на 2,
-// значит округляем количество итераций вниз, для массива из 5 элементов первой половиной будут первый и второй элементы).
-const arrayString = ['goga', 'rusla', 'slava', 'miha'];
-const middleString = Math.floor(arrayString.length / 2) - 1;
-for (let i = 0; i <= middleString; i++) {
+// Дан массив строк. Вывести только вторую половину массива (если длина не делится нацело на 2, округляем вниз,
+// для массива из 5 элементов второй половиной будет третий, четверый и пятый элементы).
+const arrayString = ['goga', 'rusla', 'slava', 'miha', 'dana'];
+const arrayLength = arrayString.length - 1;
+const middleString = Math.ceil(arrayLength / 2);
+for (let i = middleString; i < arrayString.length; i++) {
   console.log(arrayString[i]);
 }
