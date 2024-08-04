45e7d2696944cd55e31d60303c3ad2cdec4f29fa
Georgi Zarkya <zmeyfon@list.ru>
2024-08-04 21:56:33 +0300

    Подсчет

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-// Дан массив строк. Вывести только вторую половину массива (если длина не делится нацело на 2, округляем вниз,
-// для массива из 5 элементов второй половиной будет третий, четверый и пятый элементы).
-const arrayString = ['goga', 'rusla', 'slava', 'miha', 'dana'];
-const arrayLength = arrayString.length - 1;
-const middleString = Math.ceil(arrayLength / 2);
-for (let i = middleString; i < arrayString.length; i++) {
-  console.log(arrayString[i]);
+// Дан массив чисел. Необходимо посчитать количество положительных и отрицательных чисел, не учитывая последний
+const arrayNumber = [12, -123, 125, -99, 50];
+let positivNumber = 0;
+let negativNumber = 0;
+for (let i = 0; i < arrayNumber.length - 1; i++) {
+  if (arrayNumber[i] > 0) {
+    positivNumber++;
+  } else if (arrayNumber[i] < 0) {
+    negativNumber++;
+  }
 }
+console.log(positivNumber);
+console.log(negativNumber);
