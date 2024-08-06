a3146fd3b29fd77de3c4720ddbf037714ea6efa8
Georgi Zarkya <zmeyfon@list.ru>
2024-08-06 19:03:47 +0300

    Сравнение массивов

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-// Дан массив из чисел.
-// Необходимо найти среди этих чисел:
-// - Минимальное
-// - Максимальное
-// - Среднее
-const arrayNumbers = [1, 2, 100];
-let minNumber;
-let maxNumber;
-let midNumber = 0;
-for (const number of arrayNumbers) {
-  minNumber = Math.min.apply(null, arrayNumbers);
-  maxNumber = Math.max.apply(null, arrayNumbers);
-  midNumber += number / arrayNumbers.length;
+// Напишите программу, которая сравнивает 2 массива и выводит true,
+// если в них лежат одинаковые значения (даже если в разном порядке)
+const names1 = ['Ivan', 'Kovan', 'NeGogan'];
+const names2 = ['Kovan', 'Ivan', 'Gogan'];
+let namesCount = 0;
+if (names1.length === names2.length) {
+  for (const name1 of names1) {
+    for (const name2 of names2) {
+      if (name1 === name2) {
+        namesCount++;
+      }
+    }
+  }
+  if (namesCount === names2.length) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+} else {
+  console.log(false);
 }
-console.log(minNumber);
-console.log(maxNumber);
-console.log(midNumber);
