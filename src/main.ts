dce61a8c30c565d4d05844d5bda6b9a72638d297
Georgi Zarkya <zmeyfon@list.ru>
2024-08-06 19:25:40 +0300

    Ключи с четной длиной

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-// Дана строка, необходимо создать массив и записать в него каждый символ строки
-const lines = 'Goga!!!';
-const arrayText = [];
-for (const line of lines) {
-  arrayText.push(line);
+// Создайте объект с несколькими полями. Выведите все ключи этого объекта,
+// длина названия которых является четной. Используйте цикл for in
+const user = { name: 'John', email: 'john@example.com', password: '123456' };
+for (const key in user) {
+  if (!(key.length % 2)) {
+    console.log(key);
+  }
 }
-console.log(arrayText);
