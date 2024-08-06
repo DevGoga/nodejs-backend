d44962ce841584ccc2bd35fa3561e7638164f3a1
Georgi Zarkya <zmeyfon@list.ru>
2024-08-06 19:07:46 +0300

    Строка в массив

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,7 @@
-// Напишите программу, которая сравнивает 2 массива и выводит true,
-// если в них лежат одинаковые значения (даже если в разном порядке)
-const names1 = ['Ivan', 'Kovan', 'NeGogan'];
-const names2 = ['Kovan', 'Ivan', 'Gogan'];
-let namesCount = 0;
-if (names1.length === names2.length) {
-  for (const name1 of names1) {
-    for (const name2 of names2) {
-      if (name1 === name2) {
-        namesCount++;
-      }
-    }
-  }
-  if (namesCount === names2.length) {
-    console.log(true);
-  } else {
-    console.log(false);
-  }
-} else {
-  console.log(false);
+// Дана строка, необходимо создать массив и записать в него каждый символ строки
+const lines = 'Goga!!!';
+const arrayText = [];
+for (const line of lines) {
+  arrayText.push(line);
 }
+console.log(arrayText);
