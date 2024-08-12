466219631088bfb728482f414dcbc3f5a1f6892f
Georgi Zarkya <zmeyfon@list.ru>
2024-08-12 21:42:58 +0300

    Возраст Стаса

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
-// Напишите функцию, которая на вход получает объект и выдает сумму значений его полей.
-// Гарантируется, что в объекте будут содержаться только числа.
-type ObjNum = {
-  num1: any;
-  num2: any;
-  num3: any;
-};
-const numbers = {
-  num1: 123,
-  num2: undefined,
-  num3: 3,
-};
-let sum = 0;
-const funcSum = (obj: ObjNum) => {
-  for (const value of Object.values(obj)) {
-    if (typeof value === 'number') {
-      sum += value;
+// Напишите функцию, которая принимает на вход массив объектов. Ищет объект, у которого значение поля name равно Stas
+// и возвращает значение поля age  у этого объекта. Если такого объекта нет, необходимо вернуть “Объект не найден",
+// если объект найден, но поле age содержит значения null или undefined, необходимо вернуть “Возраста нет”.
+
+type ArrayObj = [
+  person1: { name?: string; age?: number },
+  person2: { name?: string; age?: number },
+  person3: { name?: string; age?: number },
+];
+const user: ArrayObj = [{ name: 'Stas' }, { age: 10 }, { name: 'Stas', age: 10 }];
+const funcSum = (obj: any) => {
+  for (let i = 0, l = obj.length; i < l; i++) {
+    if (obj[i].name && obj[i].age) {
+      return 'Возраста нет';
+    } else if (obj[i].name) {
+      return 'Объект не найден';
+    } else if (obj[i].name === 'Stas') {
+      return obj[i].age;
     }
   }
-  return sum;
 };
-
-console.log(funcSum(numbers));
+console.log(funcSum(user));
