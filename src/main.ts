92e17680b40e4452151c38d1fa92a98a0b52389c
Georgi Zarkya <zmeyfon@list.ru>
2024-08-14 23:00:09 +0300

    Bilder Message

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
-// Напишите любую осмысленную функцию, которая на вход принимает два любых осмысленных колбэка.
-// Внутри с помощью Math.random рандомит результат “успех/неуспех”,
-// и в зависимости от этого вызывает один или другой колбэк.
-const calltaxi = (open: any, close: any) => {
-  const result = Math.random();
-  if (result < 0.5) {
-    return open();
-  }
-  return close();
+// Напишите функцию, возвращающую другую функцию, чтобы этот код заработал
+const person = (word: string) => {
+  return (send: string) => {
+    const random = Math.random();
+    if (random > 0.5) {
+      return `${send}: ${word} разраб `;
+    }
+    return `${send}: ${word} не разраб`;
+  };
 };
-const showAccess = () => {
-  console.log('Showing access...');
-};
-const hideAccess = () => {
-  console.log('Hide access...');
-};
-console.log(calltaxi(showAccess, hideAccess));
+
+console.log(person('Ruslan')('goga'));
