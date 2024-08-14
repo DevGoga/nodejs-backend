7d2a627697f0e93a84e0c311ca8b1f2b807f0297
Georgi Zarkya <zmeyfon@list.ru>
2024-08-14 22:11:08 +0300

    Callback

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-// Напишите функцию, возвращающую другую функцию, чтобы этот код заработал
-const person = (word: string) => {
-  const random = Math.random();
-  if (random > 0.5) {
-    return `${word} разраб `;
-  } else return `${word} не разраб`;
+// Напишите любую осмысленную функцию, которая на вход принимает два любых осмысленных колбэка.
+// Внутри с помощью Math.random рандомит результат “успех/неуспех”,
+// и в зависимости от этого вызывает один или другой колбэк.
+const callback = () => {
+  const result = Math.random();
+  if (result < 0.5) {
+    return showAccess();
+  } else return hideAccess();
 };
-const massage = (send: string) => {
-  return `${send}: ${person('Goga')}`;
+const showAccess = () => {
+  console.log('Showing access...');
 };
-
-console.log(massage('Ruslan'));
+const hideAccess = () => {
+  console.log('Hide access...');
+};
+console.log(callback());
