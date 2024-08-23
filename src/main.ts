d965578e8daa2b2233e23ff19e45d2e3e1d381b7
Georgi Zarkya <zmeyfon@list.ru>
2024-08-23 17:16:47 +0300

    Умный поиск в массиве

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-// ### Поиск в массиве
-// Дан массив объектов, используя find, найдите объект, у которого в поле `age` лежит значение более 10
+// Доработайте функцию умного поиска, чтобы она работала как ожидается. Типы any пока оставьте.
 
-const arrayObj = [
-  { name: 'goga', age: 20 },
-  { name: 'rusla', age: 30 },
-  { name: 'slava', age: 20 },
+const storage = [
+  { age: 10, name: 'first' },
+  { age: 20, name: 'second' },
+  { age: 30, name: 'third' },
+  { age: 40, name: 'fourth' },
 ];
 
-const value = arrayObj.find((person) => person.age > 10);
-console.log(value);
+const smartSearch = (arr: any[], property: any, value: any) => {
+  return arr.find((person) => person.name === value || person.age === value);
+};
+
+const person1 = smartSearch(storage, 'age', 30);
+// { age: 30, name: 'third' }
+
+const person2 = smartSearch(storage, 'age', 10);
+// { age: 10, name: 'first' }
+
+const person3 = smartSearch(storage, 'name', 'second');
+// { age: 20, name: 'second' }
+console.log(person1, person2, person3);
