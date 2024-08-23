094d380091646a651863c732898ebfb01eb117a7
Georgi Zarkya <zmeyfon@list.ru>
2024-08-23 15:37:13 +0300

    Шифр

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
-const muliply = (a: number, b: number) => a * b;
-const sum = (a: number, b: number) => a + b;
-const substr = (a: number, b: number) => a - b;
+const original = 'the quick brown fox jumps over the lazy dog';
+const copy = 'sgd pthbj aqnvm enw itlor nudq sgd kzyx cnf';
+const convert = 'xnt gzud lzjhmf fqdzs oqnfqdrr hm kdzqmhmf izuzrbqhos. bnmfqzsr!';
+const dict: any = {};
+let result = '';
 
-const array = [
-  [1, 9, 9, muliply],
-  [2, 3, 6, sum],
-  [5, 5, 25, substr],
-];
+const word = (wordOrig: string, wordCopy: string) => {
+  for (let i = 0; i < original.length; i++) {
+    dict[wordCopy[i]] = wordOrig[i];
+  }
+  for (const text of convert) {
+    result += dict[text] ?? text;
+  }
+  return result;
+};
 
-for (const args of array) {
-  const [a, b, result, operation] = args;
-  const not = operation(a, b) === result ? '' : 'не';
-  console.log(`${not} верно`);
-}
+console.log(word(original, copy));
