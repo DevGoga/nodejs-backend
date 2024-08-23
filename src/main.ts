28135d939ff31612c18678b1a39a00bb228a493d
Georgi Zarkya <zmeyfon@list.ru>
2024-08-23 15:52:39 +0300

    Код-ревью

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
-const original = 'the quick brown fox jumps over the lazy dog';
-const copy = 'sgd pthbj aqnvm enw itlor nudq sgd kzyx cnf';
-const convert = 'xnt gzud lzjhmf fqdzs oqnfqdrr hm kdzqmhmf izuzrbqhos. bnmfqzsr!';
-const dict: any = {};
-let result = '';
-
-const word = (wordOrig: string, wordCopy: string) => {
-  for (let i = 0; i < original.length; i++) {
-    dict[wordCopy[i]] = wordOrig[i];
-  }
-  for (const text of convert) {
-    result += dict[text] ?? text;
-  }
-  return result;
+type Program = {
+  name: string;
 };
 
-console.log(word(original, copy));
+type Person = {
+  age?: number;
+  name?: string;
+  nicknames: string[];
+  programs: Program[];
+};
+
+const logProgramName = (object: Program): string => {
+  // Не сам: тут код подсветил стринг, хотя
+  // я думал, что у нас объект name типа стринг как раз и не должно быть проблем
+  console.log(object.name); //Сам: зачем нам логировать функцию, если ее и так выводит потом?
+};
+
+const logPersonName = (object: Person): string => {
+  return object.name; // Сам: вроде не сможет вернуть тип стринг, так как он необязательный и может вернуть undefind
+};
+
+const p1: Program = { name: 'programmator' };
+const p2: Person = {
+  nicknames: [], //Сам: он должен вроде ожидать массив типов стринг, а мы даем просто пустой массив
+  programs: [p1],
+};
+
+console.log(logProgramName(p1));
+console.log(logPersonName(p2));
+
+console.log(logProgramName(p1));
+console.log(logPersonName(p2)); //Сам: зачем два раза логировать одно и то же?
