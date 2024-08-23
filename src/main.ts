a9905da972b9ecbdd1b0d05925bbbfba41bfa80f
Georgi Zarkya <zmeyfon@list.ru>
2024-08-23 17:48:53 +0300

    Рандомный фильтр

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,4 @@
-// Доработайте функцию умного поиска, чтобы она работала как ожидается. Типы any пока оставьте.
-
-const storage = [
-  { age: 10, name: 'first' },
-  { age: 20, name: 'second' },
-  { age: 30, name: 'third' },
-  { age: 40, name: 'fourth' },
-];
-
-const smartSearch = (arr: any[], property: any, value: any) => {
-  return arr.find((person) => person.name === value || person.age === value);
-};
-
-const person1 = smartSearch(storage, 'age', 30);
-// { age: 30, name: 'third' }
-
-const person2 = smartSearch(storage, 'age', 10);
-// { age: 10, name: 'first' }
-
-const person3 = smartSearch(storage, 'name', 'second');
-// { age: 20, name: 'second' }
-console.log(person1, person2, person3);
+// Создай любой массив и с помощью filter возьмите из него случайные элементы (используйте Math.random())
+const array = [1, 2, 3, 4, 5];
+const convert = array.filter((num) => Math.ceil(Math.random() * 5) >= num);
+console.log(convert);
