3972767486db5fd01743d1af16a1b62ff8c8f201
Georgi Zarkya <zmeyfon@list.ru>
2024-08-23 13:38:14 +0300

    Самопроверка

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-// Напишите функцию, возвращающую другую функцию, чтобы этот код заработал
-const person = (word: string) => {
-  return (send: string) => {
-    const random = Math.random();
-    if (random > 0.5) {
-      return `${send}: ${word} разраб `;
-    }
-    return `${send}: ${word} не разраб`;
-  };
-};
+const muliply = (a: number, b: number) => a * b;
+const sum = (a: number, b: number) => a + b;
+const substr = (a: number, b: number) => a - b;
 
-console.log(person('Ruslan')('goga'));
+const array = [
+  [1, 9, 9, muliply],
+  [2, 3, 6, sum],
+  [5, 5, 25, substr],
+];
+
+for (const args of array) {
+  const [a, b, result, operation] = args;
+  const not = operation(a, b) === result ? '' : 'не';
+  console.log(`${not} верно`);
+}
