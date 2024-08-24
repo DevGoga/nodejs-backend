b9d06ad8fc915bb7e74e9cb612965ae45386b669
Georgi Zarkya <zmeyfon@list.ru>
2024-08-24 23:08:01 +0300

    reduce(оценки) - исправлено + добавлен Утилитарный тип

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,26 @@
-// Доработайте функцию умного поиска, чтобы она работала как ожидается. Типы any пока оставьте.
-
-const storage = [
-  { age: 10, name: 'first' },
-  { age: 20, name: 'second' },
-  { age: 30, name: 'third' },
-  { age: 40, name: 'fourth' },
+// Используя reduce, напишите функцию, которая превращает массив в объект. Считает количество
+// упоминаний первого числа и сумму значений для этого числа.
+type Struct = Record<string, { count: number; sum: number }>;
+const values = [
+  [1, 100],
+  [2, 200],
+  [3, 300],
+  [1, 2],
+  [3, 400],
+  [4, 500],
+  [1, 99],
 ];
 
-const smartSearch = (arr: any[], property: any, value: any) => {
-  return arr.find((person) => person[property] === value);
-};
+const result = values.reduce((acc, cur) => {
+  const [a, b] = cur;
+  const key = String(a);
+  if (!acc[key]) {
+    acc[key] = { count: 0, sum: 0 };
+  }
+  acc[key].count += 1;
+  acc[key].sum += b;
 
-const person1 = smartSearch(storage, 'age', 30);
-// { age: 30, name: 'third' }
+  return acc;
+}, {} as Struct);
 
-const person2 = smartSearch(storage, 'age', 10);
-// { age: 10, name: 'first' }
-
-const person3 = smartSearch(storage, 'name', 'second');
-// { age: 20, name: 'second' }
-console.log(person1, person2, person3);
+console.log(result);
