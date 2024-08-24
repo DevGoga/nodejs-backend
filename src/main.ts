1d06a76003134e6016b0d4bdd4e16288b765eadb
Georgi Zarkya <zmeyfon@list.ru>
2024-08-24 21:23:06 +0300

    reduce (заявки) - исправлено :)

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,21 @@
-// Доработайте функцию умного поиска, чтобы она работала как ожидается. Типы any пока оставьте.
+// Используя reduce, напишите функцию, которая превращает массив в объект. Считает количество
+// заявок в каждом статусе - отклонено, ожидание, подтверждено.
+type TicketStatus = 'approved' | 'pending' | 'declined';
 
-const storage = [
-  { age: 10, name: 'first' },
-  { age: 20, name: 'second' },
-  { age: 30, name: 'third' },
-  { age: 40, name: 'fourth' },
+type Ticket = { id: number; title: string; status: TicketStatus };
+
+const tickets: Ticket[] = [
+  { id: 1, title: 'Закупка молока, 107л', status: 'approved' },
+  { id: 2, title: 'Кошачий корм, 45т', status: 'pending' },
+  { id: 3, title: 'Свиньи, 9шт', status: 'declined' },
+  { id: 4, title: 'Дерево, 700м3', status: 'pending' },
+  { id: 5, title: 'Вода, 200т', status: 'approved' },
+  { id: 6, title: 'Сухарики, 3уп', status: 'approved' },
 ];
 
-const smartSearch = (arr: any[], property: any, value: any) => {
-  return arr.find((person) => person[property] === value || person[property] === value);
-};
-
-const person1 = smartSearch(storage, 'age', 30);
-// { age: 30, name: 'third' }
-
-const person2 = smartSearch(storage, 'age', 10);
-// { age: 10, name: 'first' }
-
-const person3 = smartSearch(storage, 'name', 'second');
-// { age: 20, name: 'second' }
-console.log(person1, person2, person3);
+const counts = tickets.reduce((acc, cur) => {
+  const key = cur.status;
+  acc[key] ? acc[key]++ : (acc[key] = 1);
+  return acc;
+}, {} as any);
+console.log(counts);
