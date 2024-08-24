19b3af4b651c784d2bae4a582ebc42cf30c1eea4
Georgi Zarkya <zmeyfon@list.ru>
2024-08-24 16:02:56 +0300

    reduce. Оценки

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,49 @@
 // Используя reduce, напишите функцию, которая превращает массив в объект. Считает количество
-// заявок в каждом статусе - отклонено, ожидание, подтверждено.
-type TicketStatus = 'approved' | 'pending' | 'declined';
-
-type Ticket = { id: number; title: string; status: TicketStatus };
-
-const tickets: Ticket[] = [
-  { id: 1, title: 'Закупка молока, 107л', status: 'approved' },
-  { id: 2, title: 'Кошачий корм, 45т', status: 'pending' },
-  { id: 3, title: 'Свиньи, 9шт', status: 'declined' },
-  { id: 4, title: 'Дерево, 700м3', status: 'pending' },
-  { id: 5, title: 'Вода, 200т', status: 'approved' },
-  { id: 6, title: 'Сухарики, 3уп', status: 'approved' },
+// упоминаний первого числа и сумму значений для этого числа.
+const values = [
+  [1, 100],
+  [2, 200],
+  [3, 300],
+  [1, 2],
+  [3, 400],
+  [4, 500],
+  [1, 99],
 ];
 
 let count1 = 0;
 let count2 = 0;
 let count3 = 0;
-const counts = tickets.reduce((acc, cur) => {
-  const key = cur.status;
-  if (cur.status === 'approved') {
+let count4 = 0;
+
+let sum1 = 0;
+let sum2 = 0;
+let sum3 = 0;
+let sum4 = 0;
+
+const result = values.reduce((acc, cur) => {
+  const key = `${cur[0]}`;
+  if (cur[0] === 1) {
     count1++;
-    acc[key] = count1;
+    sum1 += cur[1];
+    acc[key] = { count: count1, sum: sum1 };
   }
-  if (cur.status === 'declined') {
+  if (cur[0] === 2) {
     count2++;
-    acc[key] = count2;
+    sum2 += cur[1];
+    acc[key] = { count: count2, sum: sum2 };
   }
-  if (cur.status === 'pending') {
+  if (cur[0] === 3) {
     count3++;
-    acc[key] = count3;
+    sum3 += cur[1];
+    acc[key] = { count: count3, sum: sum3 };
   }
+  if (cur[0] === 4) {
+    count4++;
+    sum4 += cur[1];
+    acc[key] = { count: count4, sum: sum4 };
+  }
+
   return acc;
 }, {} as any);
-console.log(counts);
+
+console.log(result);
