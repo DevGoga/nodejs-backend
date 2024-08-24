50fef9d0d9d6c1d0a0b282c097a0cac4177bf1b0
Georgi Zarkya <zmeyfon@list.ru>
2024-08-24 13:33:48 +0300

    Обогатите список

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-// Что выведет программа ниже?
+// Доработайте код, используя метод map , чтобы он работал как ожидается:
 const storage = [
   { age: 10, name: 'first' },
   { age: 20, name: 'second' },
@@ -6,6 +6,8 @@ const storage = [
   { age: 40, name: 'fourth' },
 ];
 
-const smalls = storage.map((s) => s.age > 20);
+const storage2 = storage.map((value) => {
+  return { age: value.age, name: value.name, reversed: true };
+});
 
-console.log(smalls); // false, false, true, true
+console.log(storage2);
