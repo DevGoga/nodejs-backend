0e1edad00c9e5cd92432079c59fba41bf49309ed
Georgi Zarkya <zmeyfon@list.ru>
2024-08-26 21:21:19 +0300

    Исправьте код. Массивы

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,26 @@
-// Напишите функцию merge, которая будет производить слияние двух объектов с выбранной очередностью
-// с помощью оператора .... Доработайте код ниже, чтобы реальный вывод совпадал с ожидаемым.
-// Порядком полей в выводе можно принебречь (то есть {a:10, b:100} все равно что {b:100, a:10}).
-const merge = (a: any, b: any, order?: 1 | 2) => {
-  return order === 2 ? { ...a, ...b, merged: 'YES' } : { ...a, ...b };
-};
+// Напишите деструктуризацию, которая достаёт значения и записывает их в переменные,
+// чтобы реальный вывод программы совпадал с ожидаемым. Используйте только одну деструктуризацию
+const storage = [
+  'nick',
+  'nack',
+  'nock',
+  [
+    {
+      first: 'forecast',
+      child: null,
+    },
+    {
+      first: 'castfore',
+      child: null,
+    },
+    'zzz',
+  ],
+  'no-1',
+  'no-2',
+] as const;
 
-const obj1 = { a: 10, b: 'b', c: 100, child: { name: 'ch', surname: 'hc' } };
-const obj2 = { a: 20, b: ['b'], d: 200, child: { aa: 'aa', bb: 'bb' }, merged: 'NO!' };
+const [a, b, c, [{ first: name1, child: name2 }, b1, ...cos], ...nos] = storage;
 
-console.log(merge(obj1, obj2));
-/* Вывод:
-{
-  a: 20,
-  b: [ 'b' ],
-  c: 100,
-  child: { aa: 'aa', bb: 'bb' },
-  d: 200,
-  merged: 'NO!'
-}
-*/
-
-console.log(merge(obj1, obj2, 1));
-/* Вывод:
-{
-  a: 20,
-  b: [ 'b' ],
-  c: 100,
-  child: { aa: 'aa', bb: 'bb' },
-  d: 200,
-  merged: 'NO!'
-}
-*/
-
-console.log(merge(obj2, obj1, 2));
-/* Вывод:
-{
-  a: 10,
-  b: 'b',
-  c: 100,
-  child: { name: 'ch', surname: 'hc' },
-  d: 200,
-  merged: 'YES!'
-}
-*/
+console.log(nos);
+console.log(cos);
+console.log(b1);
