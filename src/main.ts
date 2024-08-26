cb2c4226103ffad88624ce1841e63d2f5c3fa438
Georgi Zarkya <zmeyfon@list.ru>
2024-08-26 22:48:21 +0300

    Исправьте код. Object

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,48 @@
 // Напишите деструктуризацию, которая достаёт значения и записывает их в переменные,
 // чтобы реальный вывод программы совпадал с ожидаемым. Используйте только одну деструктуризацию
-const storage = [
-  'nick',
-  'nack',
-  'nock',
-  [
-    {
-      first: 'forecast',
-      child: null,
+const megaobject = {
+  child: null,
+  child1: {
+    nick: 'name',
+    nicknames: ['n1', 'n2', { n: 'n', b: 'b' }],
+    james: {
+      kates: {
+        nicks: {
+          nicknames: ['nn', 'vv', 'gg'],
+          child: {
+            nick: 'childName',
+            child: undefined,
+          },
+        },
+      },
     },
-    {
-      first: 'castfore',
-      child: null,
+  },
+  child2: {},
+  child3: 'child3',
+} as const;
+
+const {
+  child,
+  child1: {
+    nick,
+    nicknames,
+    james: {
+      kates: {
+        nicks: {
+          nicknames: [nicknames1, ...otherNicks],
+          child: { nick: nick1, child: child1 },
+        },
+      },
     },
-    'zzz',
-  ],
-  'no-1',
-  'no-2',
-] as const;
+  },
+  ...otherChilds
+} = megaobject;
 
-const [a, b, c, [{ first: name1, child: name2 }, b1, ...cos], ...nos] = storage;
-
-console.log(nos);
-console.log(cos);
-console.log(b1);
+console.log(child); // null
+console.log(otherChilds); // { child2: {}, child3: 'child3' }
+console.log(nick); // name
+console.log(nicknames); // [ 'n1', 'n2', { n: 'n', b: 'b' } ]
+console.log(nicknames1); // nn
+console.log(otherNicks); // [ 'vv', 'gg' ]
+console.log(nick1); // childName
+console.log(child1); // undeined
