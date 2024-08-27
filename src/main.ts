4bc4a1e6c2bdbce5bd96d30294dc2c1f1c989e60
Georgi Zarkya <zmeyfon@list.ru>
2024-08-27 21:53:56 +0300

    Enum - Маппер

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 enum Colors {
   red = 'red',
-  blue = 'blue',
-  green = 'green',
-  yellow = 'yellow',
+  black = 'black',
 }
-console.log(Object.values(Colors));
+
+const mapper = (value: string): Colors => {
+  const colorsMapper = Object.values(Colors).find((color) => color === value);
+  return colorsMapper || Colors.red;
+};
+
+const color1: Colors = mapper('red'); // red, вернулось Colors.red, так как есть совпадение
+const color2: Colors = mapper('black'); // black, вернулось Colors.black, так как есть совпадение
+const color3: Colors = mapper('it is not a color'); // red, вернулось Colors.red "по умолчанию"
+
+console.log(color1, color2, color3);
