357c40d43f5fb4f5a072fb2529c13629511b89e8
Georgi Zarkya <zmeyfon@list.ru>
2024-08-27 21:22:58 +0300

    Colors

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,7 @@
-const countTypes = (...args: any[]): Record<string, number> => {
-  return args.reduce((acc, cur) => {
-    const key = typeof cur;
-    acc[key] = (acc[key] || 0) + 1;
-    return acc;
-  }, {});
-};
-
-// Просто пустая функция, чтобы её тоже передать как аргумент в countTypes
-const func = () => {};
-console.log(
-  countTypes(3, true, 'a', 1, {}, func, 4, [], undefined, false, 0, undefined, func, {}, ''),
-);
-
-// Ожидаемый вывод:
-// { number: 4, boolean: 2, string: 2, object: 3, function: 2, undefined: 2 }
+enum Colors {
+  red = 'red',
+  blue = 'blue',
+  green = 'green',
+  yellow = 'yellow',
+}
+console.log(Object.values(Colors));
