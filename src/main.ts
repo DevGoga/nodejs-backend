c3d1b29497f6f23972b314019b9041c6de9d5a9a
Georgi Zarkya <zmeyfon@list.ru>
2024-08-27 22:47:33 +0300

    Generics - Расширение

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,11 @@ type Person = {
   name: string;
 };
 
-type Paginated<T = unknown> = {
-  total: number;
-  limit: number;
-  offset: number;
-  items: T[];
+type WithId<T> = T & {
+  id: number;
 };
 
-const responseOfPersons: Paginated<Person> = {
-  total: 10,
-  limit: 5,
-  offset: 0,
-  items: [{ name: 'P1' }],
-};
-
-const responseOfSomething: Paginated = {
-  total: 10,
-  limit: 5,
-  offset: 0,
-  items: [],
+const p: WithId<Person> = {
+  id: 1,
+  name: 'name',
 };
