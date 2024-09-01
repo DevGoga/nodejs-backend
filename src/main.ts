148939ff434de33c8a0a979bd74875414da08a01
Georgi Zarkya <zmeyfon@list.ru>
2024-09-01 18:00:42 +0300

    Кодирование длин серий

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,47 @@ const user: User = {
   faaang: 'ffaanngg',
   x: 'xxxxxxxxxxxxxxxxxxxx',
 };
-const count = 0;
-const encode = (value: User) => {
-  const object = JSON.stringify(value);
-  for (const key of object) {
-    console.log(key ? object[count] : 0);
+
+const encode = <T>(decoded: T) => {
+  const object = JSON.stringify(decoded);
+  let count = 0;
+  let result = '';
+  for (let i = 0; i < object.length; i++) {
+    if (object[i] === object[i + 1]) {
+      count++;
+      continue;
+    }
+    result += object[i];
+    if (count > 1) {
+      result += count;
+    }
+    count = 1;
   }
-  return object;
+  return result;
 };
 
 const encoded: string = encode(user);
 
 console.log(encoded);
 // Строка вида "{"name": "a3bcde4f", "surname": "h2fo3dge4rg", "fa3ng": "f2a2n2g2", "x": "x20"}"
+const decode = <T>(encoded: string): T => {
+  let word = '';
+  let count = '';
+
+  for (const char of encoded) {
+    if (!isNaN(Number(char))) {
+      count += char;
+      continue;
+    }
+    if (count) {
+      word += word[word.length - 1].repeat(Number(count));
+      count = '';
+    }
+    word += char;
+  }
+
+  return JSON.parse(word);
+};
+const decoded: User = decode<User>(encoded);
+
+console.log(decoded.name, decoded.surname, decoded.faaang, decoded.x);
