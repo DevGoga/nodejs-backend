77ff076b6bc26fde9d0955fc4b58d23bd752298e
Georgi Zarkya <zmeyfon@list.ru>
2024-09-02 21:10:13 +0300

    Try, Catch , Finally ...

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,22 @@
-type User = {
-  name: string;
-  surname: string;
-  faaang: string;
-  x: string;
-};
-
-const user: User = {
-  name: 'aaabcdeeeef',
-  surname: 'hhfooodgeeeerg',
-  faaang: 'ffaanngg',
-  x: 'xxxxxxxxxxxxxxxxxxxx',
-};
-
-const encode = <T>(decoded: T) => {
-  const object = JSON.stringify(decoded);
-  let count = 0;
-  let result = '';
-  for (let i = 0; i < object.length; i++) {
-    if (object[i] === object[i + 1]) {
-      count++;
-      continue;
-    }
-    result += object[i];
-    if (count > 1) {
-      result += count;
-    }
-    count = 1;
+const balance = (value: number): any => {
+  if (value <= 0) {
+    throw Error('Balance must be greater than 0');
   }
-  return result;
-};
-
-const encoded: string = encode(user);
-
-console.log(encoded);
-// Строка вида "{"name": "a3bcde4f", "surname": "h2fo3dge4rg", "fa3ng": "f2a2n2g2", "x": "x20"}"
-const decode = <T>(encoded: string): T => {
-  let word = '';
-  let count = '';
-
-  for (const char of encoded) {
-    if (!isNaN(Number(char))) {
-      count += char;
-      continue;
-    }
-    if (count) {
-      word += word[word.length - 1].repeat(Number(count));
-      count = '';
-    }
-    word += char;
+  if (value > 1000) {
+    throw Error('Balance must be greater than 1000');
   }
-
-  return JSON.parse(word);
+  return card;
 };
-const decoded: User = decode<User>(encoded);
 
-console.log(decoded.name, decoded.surname, decoded.faaang, decoded.x);
+const card = 10000; // типа твой счёт на карте
+
+try {
+  balance(card);
+  console.log(`ваш счет на карте: ${balance(card)}`);
+} catch (e: any) {
+  if (card > 1000) {
+    console.log('Критическая ошибка!');
+    throw e;
+  }
+  console.log(`Not critical error: ${e.message}`);
+}
