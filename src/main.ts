d7aea66df48cf26fd6de4447baf2947f07a8cab4
Georgi Zarkya <zmeyfon@list.ru>
2024-09-02 23:00:14 +0300

    Generics - Рандомная выборка

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
-const balance = (value: number): any => {
-  if (value <= 0) {
-    throw Error('Balance must be greater than 0');
-  }
-  if (value > 1000) {
-    throw Error('Balance must be greater than 1000');
-  }
-  return card;
+export const generateRandomValue = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
-const card = 10000; // типа твой счёт на карте
-
-try {
-  balance(card);
-  console.log(`ваш счет на карте: ${balance(card)}`);
-} catch (e: any) {
-  if (card > 1000) {
-    console.log('Критическая ошибка!');
-    throw e;
+export const getManyRandomItems = <T>(items: T[], count: number): T[] => {
+  const arr: T[] = [];
+  for (let i = 0; i < count; i++) {
+    arr.push(getOneRandomItem(items));
   }
-  console.log(`Not critical error: ${e.message}`);
-}
+  return arr;
+};
+
+export const getOneRandomItem = <T>(items: T[]): T => {
+  return items[generateRandomValue(0, items.length)];
+};
+
+const words = ['alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot'];
+const [first, second] = getManyRandomItems(words, 2);
+console.log(first.toUpperCase()); // first должен подсвечиваться типом string
+console.log(second.toUpperCase()); // second должен подсвечиваться типом string
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const [digit] = getManyRandomItems(numbers, 1);
+console.log(digit * 100); // digit должен подсвечиваться типом number
