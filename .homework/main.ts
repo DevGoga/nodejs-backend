9bc1eb2a4debe394e14da2878a21c318239d3eeb
Georgi Zarkya <zmeyfon@list.ru>
2024-09-10 19:47:45 +0300

    Map

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,1 +1,16 @@
-console.log("Сделал домашнее задание")
+const popularity =<T> (arr: T[]): T[] => {
+  const mapper = new Map<T, number>();
+  for (const item of arr) {
+    mapper.set(item, (mapper.get(item) || 0) + 1);
+  }
+  return (Array.from(mapper.entries()).sort((a, b) => b[1] - a[1]).map(([value]) => value));
+
+
+}
+
+const arr = [8, 9, 8, 4, 9, 9];
+const arr1 = popularity(arr);
+
+console.log(arr1);
+// [9, 8, 4]
+// Чаще всего была цифра 9 (3 раза), чуть реже цифра 8 (2 раза) и т.д.
