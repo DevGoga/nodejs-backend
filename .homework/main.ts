a1326200325e8b2f0e086ded796790185be3a8b1
Georgi Zarkya <zmeyfon@list.ru>
2024-09-10 21:23:56 +0300

    Stack and Queue

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,16 +1,13 @@
-const popularity =<T> (arr: T[]): T[] => {
-  const mapper = new Map<T, number>();
-  for (const item of arr) {
-    mapper.set(item, (mapper.get(item) || 0) + 1);
-  }
-  return (Array.from(mapper.entries()).sort((a, b) => b[1] - a[1]).map(([value]) => value));
-
-
+class Stack <T> {
+  constructor(private items: T[]  = []) {}
+}
+class Queue <T> {
+  constructor(private items: T[]  = []) {}
 }
 
-const arr = [8, 9, 8, 4, 9, 9];
-const arr1 = popularity(arr);
 
-console.log(arr1);
-// [9, 8, 4]
-// Чаще всего была цифра 9 (3 раза), чуть реже цифра 8 (2 раза) и т.д.
+const stack = new Stack<number>([1, 2, 3]);
+const queue = new Queue<number>([1, 2, 3]);
+
+console.log(queue);
+console.log(stack);
