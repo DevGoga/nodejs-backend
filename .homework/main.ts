c3b2978c96be0cf72a382b8690e5bb8aa7d4dea6
Georgi Zarkya <zmeyfon@list.ru>
2024-09-15 21:42:26 +0300

    DeepStringOrUndefind

---
+++ .homework/main.ts
@@ -0,0 +1,13 @@
+type DeepString <T> = {
+  [key in keyof T]: T[key] extends object ? DeepString<T[key]> : string | undefined
+};
+
+type Person = {
+  age: number;
+  name: string;
+  son: { age: 10, name: string };
+}
+
+type GenericPerson = DeepString<Person>
+
+const obj: GenericPerson = { age: '1', name: 'John Doe', son: {age: '10', name: undefined} }
