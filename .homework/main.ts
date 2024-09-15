49c6a0f37e6090fc3c20c92c0d6b208038d0f94d
Georgi Zarkya <zmeyfon@list.ru>
2024-09-15 20:36:50 +0300

    Создайте регулярные выражения (каждый пункт - отдельное выражение) для проверки

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,16 +1,18 @@
-// удаляем слова между первыми двумя точками
-const word ='example.remove.example';
-const regexp = word.replace(/\..*?\./,'..');
-console.log(regexp);
-// извлекаем имя файла и расширение
-const word1 = "/a/b/c/d.txt";
-const regexp1 = word1.replace(/.*\//,'');
-console.log(regexp1);
-// Убрать / из конца строки
-const word2 = "https://site.com/"
-const regexp2 = word2.replace(/\/$/,'');
-console.log(regexp2);
-// Убрать ./ или / из начала строки
-const word3 = "./file.txt"
-const regexp3 = word3.replace(/^((\.\/)|(\/))/,'');
-console.log(regexp3);
+// Строка начинается с заглавной или прописной буквы A
+const check1 = new RegExp(/^а|^А/);
+// Строка кончается числом
+const check2  = new RegExp(/[0-9]$/);
+// Строка включает в себя слово `example`
+const check3 = new RegExp(/example/);
+// Строка заканчивается на `.js` или `.ts`
+const check4 = new RegExp(/\.ts$|\.js$/);
+// Строка заканчивается на `.js`, `.ts`, `.tsx` или `.jsx`
+const check5 = new RegExp(/\\.ts$|\.js$|\.jsx$|\.tsx$/);
+// Строка содержит заглавные буквы
+const check6 = new RegExp(/[A-Z]/);
+// Строка содержит только числа или маленькие буквы `c`, `d`, `e`
+const check7 = new RegExp(/^[1-9cde]+$/);
+// Строка содержит “дату”, то есть 4 цифры + дефис + 2 цифры + дефис + 2 цифры, например `2024-06-24`
+const check8 = new RegExp(/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]/);
+// Строка начинается с `folder/` , но не заканчивается на `/index.ts`, например `folder/sub-folder/file.txt`
+const check9 = new RegExp(/^folder\/(?!.*\/index\.ts$).*/);
