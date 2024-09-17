b6e4d871e45df84c3d9014830012e142773fcd0b
Georgi Zarkya <zmeyfon@list.ru>
2024-09-17 20:07:11 +0300

    Фитфти-фифти, я песок и нефертити

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,10 +1,10 @@
-type GetTypeByPosition <T extends any[], N extends number> = N extends keyof T? T[N] : undefined
+const timer = async () => {
+  const promise = await new Promise((resolve, reject) => {
+    const time = 1 + Math.round(Math.random() * 7) // с рандомом подсмотрел у тебя, каюсь) А то я так как его сделать нормально)
+    setTimeout(() => {resolve(time)}, time * 1000);
+    setTimeout(() => {reject("Я раньше")}, 4000);
+  })
+  console.log(promise);
+}
 
-type Tuple1 = [string, number];
-const nick: GetTypeByPosition<Tuple1, 0> = 'example'; // string
-const age: GetTypeByPosition<Tuple1, 1> = 200; // number
-
-type Tuple2 = [boolean, string];
-const isReady: GetTypeByPosition<Tuple2, 0> = true; // boolean
-const surname: GetTypeByPosition<Tuple2, 1> = 'example'; // string
-const bad: GetTypeByPosition<Tuple2, 5> = undefined; // undefined
+timer()
