290e81f3c8f000beefaa38a1afef20796b1bb08c
Georgi Zarkya <zmeyfon@list.ru>
2024-09-17 20:45:33 +0300

    Promise.all

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,15 +1,14 @@
-// вижу, что мы выполняем ассинхроную функцию main в котором записываем сначала текущий таймер
-// после чего по очереди выполняем чтением файлов, но перед этим читается текущий файл
-// после его прочтения мы увидим сколько читался текущий файл и та происхоит для каждого файла
-// после чего сохраняется новое время и мы выводит общее время чтения файлов
+// в коде присутствует Promise.all, что означает код будет выполняется, пока все успешном не начнут чтение
+// после того как прочитается каждый файл, он параллельно закроет чтение и выведет результат чтения для
+// каждого файла
 
 const readFile = (filename: string) => {
   const time = Math.round(Math.random() * 1000) + 500;
 
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log(`[${filename}]: Начинаю чтение`);
+    console.log(`[${filename}]: Начинаю чтение`);
 
+    setTimeout(() => {
       resolve(`Успех. Прочитали ${filename}. Время чтения: ${time}`);
 
       console.log(`[${filename}]: Закончил чтение`);
@@ -20,9 +19,11 @@ const readFile = (filename: string) => {
 const main = async () => {
   const start = new Date().getTime(); // Сохранит текущее время
 
-  const file1 = await readFile('file1');
-  const file2 = await readFile('file2');
-  const file3 = await readFile('file3');
+  const [file1, file2, file3] = await Promise.all([
+    readFile('file1'),
+    readFile('file2'),
+    readFile('file3'),
+  ]);
 
   console.log(file1);
   console.log(file2);
