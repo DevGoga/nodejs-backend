e8a6f5f921475c8437e4b2203bf1fe3903af9f1d
Georgi Zarkya <zmeyfon@list.ru>
2024-09-18 20:31:59 +0300

    Почему такой вывод?

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,19 +1,32 @@
-const sleep = (file: string, time: number) => {
-  return new Promise((resolve, reject) =>
-  {
-    if (Math.random() > 0.5) {
-      reject('Непредвиденная ошибка!');
-    }
-    setTimeout(() => {
-    resolve(`${file} Я успешно поспал!`);
-    }, time);
-  });
-}
-const main = async() => {
-  const promises = await Promise.allSettled([sleep('file1', 3000), sleep('file2', 5000)]);
-  for (const pr of promises) {
-    console.log(pr);
-  }
-}
+// До запуска кода мой ответ: startup, tick,immediate, inner immediate, timeout, inner timeout
+// но как я и думал это не правильно, на что я опирался ? startup/tick - это синхронный код, далее начинается очередь
+// из макрозадач timeout , после этого внутри setimmediate immediate, inner immediate - синхронный код,
+// после этого ставпится в очередь макрозадача  в виде inner timeout и того у нас в стеке задач =>
+// timeout и только потом inner timeout
+// После запуска кода: я ошибся в порядке с timeout, видимо он третим, так как он не расположен
+// внутри setImmediate, видимо выполнение функции setImmediate откладывается на потом, а сначала создается очередь
+// вне функций
 
-main()
+process.nextTick(() => {
+  console.log('tick');
+});
+
+setImmediate(() => {
+  console.log('immediate');
+
+  setTimeout(() => {
+    console.log('inner timeout');
+  }, 0);
+
+  setImmediate(() => {
+    console.log('inner immediate');
+  });
+});
+
+setTimeout(() => {
+  console.log('timeout');
+}, 0);
+
+const startup = 'startup';
+
+console.log(startup);
