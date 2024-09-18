c98cc385e0061a17a530020d54753e05ce16cabb
Georgi Zarkya <zmeyfon@list.ru>
2024-09-18 13:09:19 +0300

    sleep many

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,14 +1,16 @@
-const timer = 3000;
-const file = 'Rusla';
-
-
-const sleep = async (file: string, time: number) => {
-  const promise = await new Promise((resolve, reject) => {
+const sleep = (file: string, time: number) => {
+  return new Promise((resolve, reject) =>
+  {
     setTimeout(() => {
     resolve(`${file} Я поспал и прошло ${time}ms`);
     }, time);
   });
-  console.log(promise);
+}
+const main = async() => {
+  const promises = await Promise.all([sleep('file1', 3000), sleep('file2', 5000)]);
+  for (const pr of promises) {
+    console.log(pr);
+  }
 }
 
-sleep(file, timer);
+main()
