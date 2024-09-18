0cc2976c4720770da5a2ba972ed28c096653d5c1
Georgi Zarkya <zmeyfon@list.ru>
2024-09-18 21:39:54 +0300

    Что выведет код?

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,32 +1,16 @@
-// До запуска кода мой ответ: startup, tick,immediate, inner immediate, timeout, inner timeout
-// но как я и думал это не правильно, на что я опирался ? startup/tick - это синхронный код, далее начинается очередь
-// из макрозадач timeout , после этого внутри setimmediate immediate, inner immediate - синхронный код,
-// после этого ставпится в очередь макрозадача  в виде inner timeout и того у нас в стеке задач =>
-// timeout и только потом inner timeout
-// После запуска кода: я ошибся в порядке с timeout, видимо он третим, так как он не расположен
-// внутри setImmediate, видимо выполнение функции setImmediate откладывается на потом, а сначала создается очередь
-// вне функций
+// Код выводит 10 раз "10" за 1 секунду. Потому что он записывает последнее число, в нашем случае "10"
+// и после чего выводит его 10 раз за 1 секунду
 
-process.nextTick(() => {
-  console.log('tick');
-});
+var i = 0;
+const main = async() => {
+  for (i = 0; i < 10; i++) {
+    await new Promise((resolve) => {
+      setTimeout(function() {
+        console.log(i);
+        resolve(true);
+      }, 1000);
+    });
+  }
+}
 
-setImmediate(() => {
-  console.log('immediate');
-
-  setTimeout(() => {
-    console.log('inner timeout');
-  }, 0);
-
-  setImmediate(() => {
-    console.log('inner immediate');
-  });
-});
-
-setTimeout(() => {
-  console.log('timeout');
-}, 0);
-
-const startup = 'startup';
-
-console.log(startup);
+main()
