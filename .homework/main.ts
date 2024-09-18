7502d4f091f9f0c7c9d15cba63429f7feb96febd
Georgi Zarkya <zmeyfon@list.ru>
2024-09-18 13:14:57 +0300

    sleep many info

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,13 +1,16 @@
 const sleep = (file: string, time: number) => {
   return new Promise((resolve, reject) =>
   {
+    if (Math.random() > 0.5) {
+      reject('Непредвиденная ошибка!');
+    }
     setTimeout(() => {
-    resolve(`${file} Я поспал и прошло ${time}ms`);
+    resolve(`${file} Я успешно поспал!`);
     }, time);
   });
 }
 const main = async() => {
-  const promises = await Promise.all([sleep('file1', 3000), sleep('file2', 5000)]);
+  const promises = await Promise.allSettled([sleep('file1', 3000), sleep('file2', 5000)]);
   for (const pr of promises) {
     console.log(pr);
   }
