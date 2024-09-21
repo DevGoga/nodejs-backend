b794eace3c5127991b4f3b8499cc1ee9793efc6b
Georgi Zarkya <zmeyfon@list.ru>
2024-09-21 21:56:34 +0300

    Punycode :) (прочитал про него)

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,4 @@
 // http://gogma/русский.com закодирова, как: http%3A%2F%2Fgogma%2F%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9.com
 // соответственно, http%3A%2F%2Fgogma%2F%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9.com декодировал, как
 // http://gogma/русский.com
+// прочитал punycode
