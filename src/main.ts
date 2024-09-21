6c0e5f7b7e4b9ed76a2fd264884abdf48b9c3f89
Georgi Zarkya <zmeyfon@list.ru>
2024-09-21 21:52:09 +0300

    URL Encoding

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,4 @@
-import express from 'express';
-
-const server = express();
-
-const port = 3000;
-
-server.listen(port);
-server.get('/end', (req, res) => {
-  res.end('ты используешь end!');
-}); // использовал Postman и в ответе он просто отправил данные
-
-server.get('/send', (req, res) => {
-  res.send('ты используешь send!');
-}); //использовал Postman и в ответе он отправил данные, которые соответсвует типу "text"
-server.get('/json', (req, res) => {
-  res.json('ты используешь json!');
-}); //использовал Postman и в ответе он отправил данные в виде json
+// используя https://www.urldecoder.org/
+// http://gogma/русский.com закодирова, как: http%3A%2F%2Fgogma%2F%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9.com
+// соответственно, http%3A%2F%2Fgogma%2F%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9.com декодировал, как
+// http://gogma/русский.com
