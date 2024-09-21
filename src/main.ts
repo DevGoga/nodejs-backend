1ee353fc0d035dc5a82f4cd04d00812a5b824e30
Georgi Zarkya <zmeyfon@list.ru>
2024-09-21 20:11:24 +0300

    Тренировка

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import express from 'express';
 
 const server = express();
 
-const port = 2000;
+const port = 3000;
 server.listen(port);
+server.get('/end', (req, res) => {
+  res.end('ты используешь end!');
+}); // использовал Postman и в ответе он просто отправил данные
+
+server.get('/send', (req, res) => {
+  res.send('ты используешь send!');
+}); //использовал Postman и в ответе он отправил данные, которые соответсвует типу "text"
+server.get('/json', (req, res) => {
+  res.json('ты используешь json!');
+}); //использовал Postman и в ответе он отправил данные в виде json
