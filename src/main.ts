a5d8dcd9a8ed9a1e106fcd626a1e442afafc1f39
Georgi Zarkya <zmeyfon@list.ru>
2024-09-21 18:55:36 +0300

    Первые шаги

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,27 @@ import express from 'express';
 
 const server = express();
 
-const port = 2000;
-server.listen(port);
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+server.get('/admin', (req, res) => {
+  console.log('Пришел запрос с методом GET');
+  res.send('Hello World!');
+});
+
+server.post('/', (req, res) => {
+  console.log('Пришел запрос с методом POST');
+  res.send('Hello World!');
+});
+
+server.put('/', (req, res) => {
+  console.log('Пришел запрос с методом PUT');
+  res.send('Hello World!');
+});
+
+server.delete('/', (req, res) => {
+  console.log('Пришел запрос с методом DELETE');
+  res.send('Hello World!');
+});
