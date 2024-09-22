0d1a26f18dcd2ecf96a4d5bd1a6dc16455c7573a
Georgi Zarkya <zmeyfon@list.ru>
2024-09-22 19:30:33 +0300

    Training

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,58 @@
 import express from 'express';
 const server = express();
-const port = 3100;
+const port = 4000;
 
-server.listen(port, () => {
-  return 'Успешно запущен!';
+const usersRouter = express.Router();
+const newsRouter = express.Router();
+const statisticsRouter = express.Router();
+const settingsRouter = express.Router();
+
+usersRouter.get('', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
+});
+usersRouter.get('/:id', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
+});
+usersRouter.post('/:id', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
+});
+usersRouter.put('/:id', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
 });
 
-server.use(express.json());
-
-server.post('/payload', (req, res) => {
-  const params = req.params;
-  const query = req.query;
-  const body = req.body;
-  return res.json({ params, query, body });
+newsRouter.get('/:id', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
 });
+newsRouter.post('/:id', (req, res) => {
+  return res.send(`пришло ${req.params}`);
+});
+newsRouter.put('/:id', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
+});
+newsRouter.delete('/:id', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
+});
+
+settingsRouter.get('', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
+});
+settingsRouter.get('/all', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
+});
+settingsRouter.put('/:id', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
+});
+settingsRouter.delete('/:id/:subId', (req, res) => {
+  return res.send(`пришло: ${req.params}`);
+});
+
+statisticsRouter.get('/:group/ready-list', () => {});
+statisticsRouter.delete('/:group/ready/:name', () => {});
+statisticsRouter.post('/:group/ready/:name/create', () => {});
+
+server.use('/users', usersRouter);
+server.use('/news', usersRouter);
+server.use('/statistics', statisticsRouter);
+server.use('/users/settings', settingsRouter);
+
+server.listen(port);
