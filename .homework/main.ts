3985c6657aa1ee657e3f2318e85099e41d93ae6b
Georgi Zarkya <zmeyfon@list.ru>
2024-09-25 19:25:34 +0300

    EventEmitter

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,1 +1,8 @@
+import { EventEmitter } from 'node:events';
 
+const emitter = new EventEmitter();
+emitter.on('error', () => console.log('Вызвался error!'));
+emitter.on('end', () => console.log('Вызвался end!'));
+if (Math.random() > 0.5) {
+  emitter.emit('error');
+} else emitter.emit('end');
