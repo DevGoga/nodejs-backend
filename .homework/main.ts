d61e50f4ece550001640e6e81d976d31fd062be1
Georgi Zarkya <zmeyfon@list.ru>
2024-09-27 18:46:46 +0300

    Наследование

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,8 +1,25 @@
-import { EventEmitter } from 'node:events';
+import {EventEmitter} from 'node:events'
+class Kitchen extends EventEmitter{
+   cookingPizza() {
+    this.emit('начал приготовление');
+    try {
+      this.emit('создание теста');
+      this.emit('добавление ингридиентов')
+      this.emit('добавить в печь')
+      setTimeout(() => this.emit('пицца готова!'), 3000)
+    }
+    catch(err) {
+      console.error('sorry, i dont understand', err);
+    }
+  }
+}
+const kitchen = new Kitchen();
 
-const emitter = new EventEmitter();
-emitter.on('error', () => console.log('Вызвался error!'));
-emitter.on('end', () => console.log('Вызвался end!'));
-if (Math.random() > 0.5) {
-  emitter.emit('error');
-} else emitter.emit('end');
+kitchen.on('начал приготовление', () => console.log('приготовление началось!'));
+kitchen.on('создание теста', () => console.log('создал тесто!'));
+kitchen.on('добавление ингридиентов', () => console.log('добавил ингридиенты!'));
+kitchen.on('добавить в печь', () => console.log('добавил пиццу в печь!'));
+kitchen.on('пицца готова!', () => console.log('я приготовил пиццу!!!)'));
+kitchen.on('sorry, i dont understand', () => console.log('я хз как готовить :('));
+
+kitchen.cookingPizza();
