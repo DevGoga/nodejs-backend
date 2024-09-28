e88c31f08c0127dd3e6df8de51332e0d4d2c24e6
Georgi Zarkya <zmeyfon@list.ru>
2024-09-28 21:16:56 +0300

    Legacy

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,27 +1,33 @@
-import {EventEmitter} from 'node:events'
-class Emitter extends EventEmitter {
-  makeBurger() {
-    this.emit('бургер');
-  };
-  makePizza(){
-    this.emit('пицца');
-  }
-  makePasta(){
-    this.emit("паста");
-  }
+import { EventEmitter } from 'node:events';
+enum Cook {
+  start = 'начал приготовление',
+  create = 'создание теста',
+  add = 'добавление ингридиентов',
+  oven = 'добавить в печь',
+  finish = 'пицца готова!',
+  error = 'sorry, i dont understand',
 }
-class Kitchen {
-   cookBurger() {console.log('делаю бургер')};
-   cookPizza() {console.log('делаю пиццу')};
-   cookPasta(){console.log('делаю пасту')};
+class Kitchen extends EventEmitter {
+  async cookingPizza() {
+    this.emit(Cook.start);
+    this.emit(Cook.create);
+    this.emit(Cook.add);
+    this.emit(Cook.oven);
+    new Promise((resolve, reject) => {
+      if (Math.random() > 0.5) {
+        return reject(this.emit(Cook.error));
+      }
+      resolve(this.emit(Cook.finish));
+    });
+  }
 }
 const kitchen = new Kitchen();
-const eventEmitter = new Emitter();
 
-eventEmitter.on('бургер', () => {kitchen.cookBurger()});
-eventEmitter.on('пицца', () => {kitchen.cookPizza()});
-eventEmitter.on('паста', () => {kitchen.cookPasta()});
+kitchen.on('начал приготовление', () => console.log('приготовление началось!'));
+kitchen.on('создание теста', () => console.log('создал тесто!'));
+kitchen.on('добавление ингридиентов', () => console.log('добавил ингридиенты!'));
+kitchen.on('добавить в печь', () => console.log('добавил пиццу в печь!'));
+kitchen.on('пицца готова!', () => console.log('я приготовил пиццу!!!'));
+kitchen.on('sorry, i dont understand', () => console.log('я хз как готовить :('));
 
-eventEmitter.makeBurger();
-eventEmitter.makePizza();
-eventEmitter.makePasta();
+kitchen.cookingPizza();
