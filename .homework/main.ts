7b36e41a5ed757b0a1bbacb98ab104ffab7a0fb0
Georgi Zarkya <zmeyfon@list.ru>
2024-09-28 21:20:09 +0300

    Documentation(reading)

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,33 +1,6 @@
-import { EventEmitter } from 'node:events';
-enum Cook {
-  start = 'начал приготовление',
-  create = 'создание теста',
-  add = 'добавление ингридиентов',
-  oven = 'добавить в печь',
-  finish = 'пицца готова!',
-  error = 'sorry, i dont understand',
-}
-class Kitchen extends EventEmitter {
-  async cookingPizza() {
-    this.emit(Cook.start);
-    this.emit(Cook.create);
-    this.emit(Cook.add);
-    this.emit(Cook.oven);
-    new Promise((resolve, reject) => {
-      if (Math.random() > 0.5) {
-        return reject(this.emit(Cook.error));
-      }
-      resolve(this.emit(Cook.finish));
-    });
-  }
-}
-const kitchen = new Kitchen();
-
-kitchen.on('начал приготовление', () => console.log('приготовление началось!'));
-kitchen.on('создание теста', () => console.log('создал тесто!'));
-kitchen.on('добавление ингридиентов', () => console.log('добавил ингридиенты!'));
-kitchen.on('добавить в печь', () => console.log('добавил пиццу в печь!'));
-kitchen.on('пицца готова!', () => console.log('я приготовил пиццу!!!'));
-kitchen.on('sorry, i dont understand', () => console.log('я хз как готовить :('));
-
-kitchen.cookingPizza();
+// ### Документация
+//https://docs.github.com/en/actions/about-github-actions/understanding-github-actions
+//https://docs.github.com/en/actions
+//https://docs.github.com/en/actions/writing-workflows/workflow-syntax-for-github-actions#jobsjob_idruns-on
+//https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/storing-workflow-data-as-artifacts
+//https://github.com/marketplace?type=actions - Actions Marketplace
