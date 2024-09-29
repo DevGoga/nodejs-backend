6a0bc91d5b9db2a804d4c40a6285f8dd59561ba6
Georgi Zarkya <zmeyfon@list.ru>
2024-09-29 20:25:45 +0300

    Review

--- .homework/main.ts
+++ .homework/main.ts
@@ -1,6 +1,46 @@
-// ### Документация
-//https://docs.github.com/en/actions/about-github-actions/understanding-github-actions
-//https://docs.github.com/en/actions
-//https://docs.github.com/en/actions/writing-workflows/workflow-syntax-for-github-actions#jobsjob_idruns-on
-//https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/storing-workflow-data-as-artifacts
-//https://github.com/marketplace?type=actions - Actions Marketplace
+/*
+  Исправьте фрагмент программы, чтобы она работала как ожидается
+ */
+
+type WebMaster = {
+  nick: string;
+  followers: number;
+  isPro: boolean;
+  photo: string | null;
+};
+
+export const separator = '\t';
+
+export const write = (master: WebMaster, separator: string) => {
+  const { isPro, followers, nick, photo } = master;
+
+  return [nick, followers, isPro, photo].map(String).join(separator);
+};
+
+export const parse = (raw: string, separator: string): WebMaster => {
+  const [nick, followers, isPro, photo] = raw.split(separator);
+
+  return {
+    nick,
+    followers: Number(followers),
+    isPro: isPro === 'true',
+    photo,
+  };
+};
+
+const webmaster1: WebMaster = { nick: 'first', followers: 1, isPro: true, photo: null };
+const webmaster2: WebMaster = { nick: 'second', followers: 2, isPro: false, photo: 'img-2' };
+
+const rawWm1 = write(webmaster1, separator);
+const rawWm2 = write(webmaster2, separator);
+
+const parsedWm1 = parse(rawWm1, separator);
+const parsedWm2 = parse(rawWm2, separator);
+
+console.log(parsedWm1);
+// Ожидаемый вывод: { nick: 'first', followers: 1, isPro: true, photo: null }
+// Текущий вывод: { nick: 'first\t1\ttrue\tnull', followers: NaN, isPro: false, photo: undefined }
+
+console.log(parsedWm2);
+// Ожидаемый вывод: { nick: 'second', followers: 2, isPro: false, photo: 'img-2' }
+// Текущий вывод: { nick: 'second\t2\tfalse\timg-2', followers: NaN, isPro: false, photo: undefined }
