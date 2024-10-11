feca676b364622713a803785fc1066832b3de84b
Georgi Zarkya <zmeyfon@list.ru>
2024-10-11 20:02:57 +0300

    feat: rateLimiter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 
 import { logRoutes } from './bootstrap';
+import { rateLimiter } from './middlewares/rateLimiter';
 import { taskRouter, userRouter } from './modules';
 
 const server = express();
-const port = 4000;
+
+const port = 2000;
+
+server.use(rateLimiter);
 server.use('/user', userRouter);
 server.use('/task', taskRouter);
+
 logRoutes(server);
+
 server.listen(port, () => console.log(`Server started on port ${port}`));
