a692e4a53daaa5dd8378a5ac8cb05a8f8e2d8e81
Georgi Zarkya <zmeyfon@list.ru>
2024-10-17 00:58:17 +0300

    feat: userLogin.dto.ts

--- src/modules/user.router.ts
+++ src/modules/user.router.ts
@@ -1,8 +1,26 @@
+import { plainToInstance } from 'class-transformer';
+import { validateSync } from 'class-validator';
 import express from 'express';
+import { BadRequestException } from '../../express/src/errors';
+import { UserLoginDto } from '../dto/userLogin.dto';
+
 export const userRouter = express.Router();
 
 userRouter.get('/login', (req, res) => {
-  res.send('get login');
+  const dto = plainToInstance(UserLoginDto, req.params);
+  const errors = validateSync(dto);
+  if (errors.length) {
+    const constraints = errors[0].constraints;
+    let message = 'Unknown validation error';
+
+    if (constraints) {
+      message = constraints[Object.keys(constraints)[0]];
+    }
+
+    throw new BadRequestException(message);
+  }
+
+  res.status(200).json('Login success!');
 });
 userRouter.post('/register', (req, res) => {
   res.send('create new user');
