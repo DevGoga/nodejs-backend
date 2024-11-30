a947539f4cfd3cc8e8fb593c6c3d3bcff562d3bb
Georgi Zarkya <zmeyfon@list.ru>
2024-11-30 22:02:43 +0300

    fix/incorrect-output

--- src/main.ts
+++ src/main.ts
@@ -1,120 +1,47 @@
-/*
-Создайте класс MyStorage<T>, который необходим для хранения и работы с объектами типа T.
-Внутри этот класс должен хранить элементы в приватном массиве.
-Для работы с элементами должны существовать 4 публичных метода:
-* create - принимает на вход объект, добавляет к нему поле id - уникальный идентификатор,
-  он не должен пересекаться с теми id, которые уже есть в хранилище.
-  Проще всего использовать числовой id и увеличивать его - 1, 2, 3, 4, ... и так далее
-* read - принимает на вход id и возвращает из хранилища элемент с этим id, или undefined, если такого элемента нет
-* delete - принимает на вход id и удаляет из хранилища элемент с этим id, ничего не возращает
-* update - принимает на вход id и частичную сущность. Ищет в хранилище элемент с таким id и обновляет его.
-
-Так же для тестов необходимо предоставить публичный метод getStorage(), который будет возвращать использующийся массив
-
-Чтобы проверить работоспособность кода, необходимо просто его запустить.
-Если что-то не работает, вы увидите ошибку "It is no working as expected"
-Если всё работает, вы увидите "Всё работает!"
- */
-
-class MyStorage<T> {
-  private storage: (T & { id: number })[] = [];
-  private currentId: number = 1;
-
-  create(item: T) {
-    const newItem = { ...item, id: this.currentId };
-    this.storage.push(newItem);
-    this.currentId++;
-    return newItem;
-  }
-
-  read(id: number): (T & { id: number }) | undefined {
-    return this.storage.find((item) => item.id === id);
-  }
-
-  delete(id: number) {
-    this.storage = this.storage.filter((item) => item.id !== id);
-  }
-
-  update(id: number, partialItem: Partial<T>) {
-    const index = this.storage.findIndex((item) => item.id === id);
-    this.storage[index] = { ...this.storage[index], ...partialItem };
-    return this.storage[index];
-  }
-
-  getStorage(): (T & { id: number })[] {
-    return this.storage;
-  }
-}
-
-type User = {
-  name: string;
-  age: number;
+type GroupSettings = {
+  startFrom: number;
+  maxElements: number;
 };
 
-const storage = new MyStorage<User>();
-storage.create({ name: 'first', age: 1 });
-storage.create({ name: 'second', age: 2 });
-storage.create({ name: 'third', age: 3 });
+const defaultSettings: GroupSettings = { startFrom: 0, maxElements: 3 };
 
-storage.delete(2);
+const words1: string[] = [
+  'banana',
+  'cat',
+  'elephant',
+  'apple',
+  'dog',
+  'carrot',
+  'house',
+  'sun',
+  'moon',
+  'star',
+  'tree',
+  'book',
+  'flower',
+  'coffee',
+  'ocean',
+];
 
-storage.create({ name: 'fourth', age: 4 });
+const words2: string[] = ['java', 'code', 'book', 'boss', 'moon', 'face', 'tree', 'bird', 'fire', 'read'];
 
-let updatedFirst;
-updatedFirst = storage.update(1, { name: 'first-new-name' });
-updatedFirst = storage.update(1, {});
-updatedFirst = storage.update(1, { age: 11 });
+const group = (items: string[], customSettings?: GroupSettings) => {
+  const settings = customSettings ?? defaultSettings;
+  const linesCount = Math.ceil(items.length / settings.maxElements);
 
-const updatedFourth = storage.update(4, { age: 44, name: 'fourth-new-name' });
+  console.log(`Будет выведено ${linesCount} строки`);
 
-const user1 = storage.read(1);
-const user3 = storage.read(3);
-const user5 = storage.read(5);
+  for (let i = settings.startFrom; i < linesCount; i++) {
+    const start = i * settings.maxElements;
+    const end = start + settings.maxElements;
 
-// На этих строчках должны гореть ошибки! Для запуска кода закомментируйте их!
-// storage.update(1000, { age: 'a' }); // Должна гореть ошибка, что поле age должно являться числом
-// storage.update(1000, { x: true }); // Должна гореть ошибка, что поля x нет в User
-
-// Тест-кейсы
-const th = () => {
-  throw Error('It is no working as expected');
+    const elements = items.slice(start, end);
+    console.log(`Строка ${i + 1}:`, elements);
+  }
 };
-const [item1, item2, item3, item4] = storage.getStorage();
 
-if (item1.id !== 3 || item1.age !== 3 || item1.name !== 'third') {
-  th();
-}
+group(words1);
 
-if (item2.id !== 1 || item2.age !== 11 || item2.name !== 'first-new-name') {
-  th();
-}
+console.log();
 
-if (item3.id !== 4 || item3.age !== 44 || item3.name !== 'fourth-new-name') {
-  th();
-}
-
-if (item4 !== undefined) {
-  th();
-}
-
-if (updatedFirst.id !== 1 || updatedFirst.age !== 11 || updatedFirst.name !== 'first-new-name') {
-  th();
-}
-
-if (updatedFourth.id !== 4 || updatedFourth.age !== 44 || updatedFourth.name !== 'fourth-new-name') {
-  th();
-}
-
-if (user1?.id !== 1 || user1?.age !== 11 || user1?.name !== 'first-new-name') {
-  th();
-}
-
-if (user3?.id !== 3 || user3?.age !== 3 || user3?.name !== 'third') {
-  th();
-}
-
-if (user5 !== undefined) {
-  th();
-}
-
-console.log('Всё работает!');
+group(words2);
